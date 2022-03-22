42f51b987ac2d91e002437a9162e7f6103826565
Shape Ous <shapeous@yahoo.com>
2022-03-22 14:14:53 -0400

    Creating PHP variables

--- experiment.php
+++ experiment.php
@@ -1,1 +1,18 @@
-This is a test
+<?php
+  $title = "Shapey Experiments";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?php echo $title ?></title>
+  <!--link rel="stylesheet" href="css/styles.css?v=1.0"-->
+</head>
+
+<body>
+  <h1><?php echo $title ?></h1>
+  <!--script src="js/scripts.js"></script-->
+</body>
+</html>
