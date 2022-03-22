5041d0aa089c62680811cffe14731024835bf6c0
Shape Ous <shapeous@yahoo.com>
2022-03-22 16:17:00 -0400

    Using WordPress defined functions

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ This is just a sandbox for learning traditional WordPress stuff.
 ## Creating our own custom theme
   - Name: shapey-university-theme
   - Path: "~\Local Sites\shapey-university\app\public\wp-content\themes\shapey-university-theme"
+
+## Functions
+  - WordPress Functions:
+    - bloginfo('name') - Defined under wp-admin > Settings > Site Title
+    - bloginfo('description') - Defined under wp-admin > Settings > Tagline
--- themes/shapey-university-theme/index.php
+++ themes/shapey-university-theme/index.php
@@ -1,1 +1,24 @@
-This is our amazing custom theme!
+<?php
+  function greet($name, $color) {
+    echo "<p>Hello, my name is $name and my favorite color is $color!</p>";
+  }
+
+  greet("Shape", "green");
+  greet("Octavio", "purple");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?php bloginfo('name') ?></title>
+  <!--link rel="stylesheet" href="css/styles.css?v=1.0"-->
+</head>
+
+<body>
+  <h1><?php bloginfo('name') ?></h1>
+  <h2><?php bloginfo('description') ?></h2>
+  <!--script src="js/scripts.js"></script-->
+</body>
+</html>
